f9e8dd5d068a0e46cae61c8a995eb51b4b86681e
PalinaHaidzina <palina.h@gofarsoft.org>
2024-08-28 11:06:38 +0300

    add get cryptogram method

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,24 @@
+import {
+  cryptogramECDHValueData,
+  MockCryptogramPaymentDataFFD1_05, MockCryptogramPaymentDataFFD1_2,
+  MockPayResponse,
+  MockTransactionStateWaitFor3ds,
+  MockTransactionStatus,
+} from "@/constants/mockData";
 import { enc, HmacSHA256 } from "crypto-js";
 import { Formik } from "formik";
 import paymentApi from "payselection-pay-app-sdk-reactnative/src/api/payment";
 import getStatusApi from "payselection-pay-app-sdk-reactnative/src/api/status";
 import {
-  AgentInfoType,
-  PaymentMethodType,
-  PaymentObjectFFD1_05,
-  PaymentObjectFFD1_2,
-  PaymentsType,
-  TaxSystem,
-  VatType,
-} from "payselection-pay-app-sdk-reactnative/src/types/commonTypes";
+  getCryptogramECDHValue,
+  getCryptogramRSAValue,
+} from "payselection-pay-app-sdk-reactnative/src/types/payment/cryptoUtils";
 import {
+  CryptogramPayment,
   CryptogramRSAPayment,
   PublicPayHeader,
 } from "payselection-pay-app-sdk-reactnative/src/types/payment/paymentPayload";
 import { PayResponse } from "payselection-pay-app-sdk-reactnative/src/types/payment/paymentResponse";
-import { getCryptogramValue } from "payselection-pay-app-sdk-reactnative/src/types/payment/utils";
 import {
   GetStatusByOrderIdHeader,
   GetStatusByTransactionIdHeader,
@@ -34,7 +36,7 @@ import ScrollView = Animated.ScrollView;
 
 const payHeader: PublicPayHeader = {
   X_SITE_ID: '21044',
-  X_REQUEST_ID: '4f6b71f1-ddfd-489b-b855-480ebb255612v',
+  X_REQUEST_ID: '4f6b71f1-ddfd-489b-b855-Testq2',
 }
 
 const MockSignatureTransaction: SignatureProps = {
@@ -89,298 +91,6 @@ const signatureGeneration = ( {
   return HmacSHA256( signature_string, siteSecretKey ).toString( enc.Hex );
 }
 
-
-const MockCryptogramPaymentDataFFD1_05: CryptogramRSAPayment = {
-  OrderId: "SAM_SDK_3",
-  Amount: "11.00",
-  Currency: "RUB",
-  Description: "test payment",
-  RebillFlag: false,
-  CustomerInfo: {
-    Email: "user@example.com",
-    Phone: "+19991231212",
-    Language: "en",
-    Address: "string",
-    Town: "string",
-    ZIP: "1234567",
-    Country: "USA",
-    IP: "10.0.2.56",
-    UserId: "string"
-  },
-  ExtraData: {
-    ReturnUrl: "https://api.payselection.com/",
-    WebhookUrl: "https://webhook.site/94a06b69",
-    ScreenHeight: "768",
-    ScreenWidth: "1024",
-    ChallengeWindowSize: "5",
-    TimeZoneOffset: "-180",
-    ColorDepth: "8",
-    Region: "ru",
-    UserAgent: "Mozilla/5.0+(Macintosh;+Intel+Mac+OS+X+10_15_5)+AppleWebKit/527.36+(KHTML,+Gecko)+Chrome83.0.4103.116+Safari/537.36",
-    acceptHeader: "text/html",
-    JavaEnabled: true,
-    javaScriptEnabled: true
-  },
-  ReceiptData: {
-    timestamp: "string",
-    external_id: "string",
-    receipt: {
-      client: {
-        name: "string",
-        inn: "string",
-        email: "string",
-        phone: "string"
-      },
-      company: {
-        email: "string",
-        sno: TaxSystem.osn,
-        inn: "string",
-        payment_address: "string",
-      },
-      agent_info: {
-        type: AgentInfoType.bank_paying_agent,
-        paying_agent: {
-          operation: "string",
-          phones: ["+375298763261"],
-        },
-        receive_payments_operator: {
-          phones: ["+375298763262"]
-        },
-        money_transfer_operator: {
-          phones: ["+375298763263"],
-          name: "string",
-          address: "string",
-          inn: "string"
-        }
-      },
-      supplier_info: {
-        phones: ["375298763264"]
-      },
-      items: [
-        {
-          name: "string",
-          price: 42949673,
-          quantity: 99999.999,
-          sum: 42949672.95,
-          measurement_unit: "string",
-          payment_method: PaymentMethodType.full_payment,
-          payment_object: PaymentObjectFFD1_05.commodity,
-          nomenclature_code: "string",
-          vat: {
-            type: VatType.vat10,
-            sum: 99999999.99,
-          },
-          agent_info: {
-            type: AgentInfoType.bank_paying_agent,
-            paying_agent: {
-              operation: "string",
-              phones: ["+375441238751"]
-            },
-            receive_payments_operator: {
-              phones: ["+375441238752"]
-            },
-            money_transfer_operator: {
-              phones: ["+375441238734"],
-              name: "string",
-              address: "string",
-              inn: "string",
-            },
-          },
-          supplier_info: {
-            phones: ["+375441238731"],
-            name: "string",
-            inn: "string",
-          },
-          user_data: "string",
-          excise: 0,
-          country_code: "str",
-          declaration_number: "string",
-        }
-      ],
-      payments: [{
-        type: PaymentsType.cash,
-        sum: 99999999.99,
-      }],
-      vats: [{
-        type: VatType.vat0,
-        sum: 99999999.99,
-      }],
-      total: 99999999.99,
-      additional_check_props: "string",
-      cashier: "string",
-      additional_user_props: {
-        name: "string",
-        value: "string"
-      }
-    }
-  },
-  PaymentDetails: {
-    Value: "",
-  },
-  PaymentMethod: "CryptogramRSA",
-}
-
-const MockCryptogramPaymentDataFFD1_2: CryptogramRSAPayment = {
-  OrderId: "SAM_SDK_3",
-  Amount: "11.00",
-  Currency: "RUB",
-  Description: "test payment",
-  RebillFlag: false,
-  CustomerInfo: {
-    Email: "user@example.com",
-    Phone: "+19991231212",
-    Language: "en",
-    Address: "string",
-    Town: "string",
-    ZIP: "1234567",
-    Country: "USA",
-    IP: "10.0.2.56",
-    UserId: "string"
-  },
-  ExtraData: {
-    ReturnUrl: "https://api.payselection.com/",
-    WebhookUrl: "https://webhook.site/94a06b69",
-    ScreenHeight: "768",
-    ScreenWidth: "1024",
-    ChallengeWindowSize: "5",
-    TimeZoneOffset: "-180",
-    ColorDepth: "8",
-    Region: "ru",
-    UserAgent: "Mozilla/5.0+(Macintosh;+Intel+Mac+OS+X+10_15_5)+AppleWebKit/527.36+(KHTML,+Gecko)+Chrome83.0.4103.116+Safari/537.36",
-    acceptHeader: "text/html",
-    JavaEnabled: true,
-    javaScriptEnabled: true
-  },
-  ReceiptData: {
-    timestamp: "string",
-    external_id: "string",
-    receipt: {
-      client: {
-        name: "string",
-        inn: "string",
-        email: "string",
-        phone: "string"
-      },
-      company: {
-        email: "string",
-        sno: TaxSystem.osn,
-        inn: "string",
-        paymentAddress: "string",
-      },
-      items: [
-        {
-          name: "string",
-          price: 42949673,
-          quantity: 99999.999,
-          sum: 42949672.95,
-          measure: 0,
-          payment_method: PaymentMethodType.full_payment,
-          payment_object: PaymentObjectFFD1_2.pawn,
-          vat: {
-            type: VatType.vat10,
-            sum: 99999999.99,
-          },
-          agent_info: {
-            type: AgentInfoType.bank_paying_agent,
-            paying_agent: {
-              operation: "string",
-              phones: ["+375441238751"]
-            },
-            receive_payments_operator: {
-              phones: ["+375441238752"]
-            },
-            money_transfer_operator: {
-              phones: ["+375441238734"],
-              name: "string",
-              address: "string",
-              inn: "string",
-            },
-          },
-          supplier_info: {
-            phones: ["+375441238731"],
-            name: "string",
-            inn: "string",
-          },
-          user_data: "string",
-          excise: 0,
-          country_code: "str",
-          declaration_number: "string",
-          mark_quantity: {
-            numerator: 2,
-            denominator: 2
-          },
-          mark_processing_mode: "string",
-          sectoral_item_props: [{
-            federal_id: "string",
-            date: "string",
-            number: "string",
-            value: "string"
-          }],
-          mark_code: {
-            unknown: "string",
-            ean: "string",
-            ean13: "string",
-            itf14: "string",
-            gs10: "string",
-            gs1m: "string",
-            short: "string",
-            fur: "string",
-            egais20: "string",
-            egais30: "string",
-          }
-        }
-      ],
-      payments: [{
-        type: PaymentsType.cash,
-        sum: 99999999.99,
-      }],
-      vats: [{
-        type: VatType.vat0,
-        sum: 99999999.99,
-      }],
-      total: 99999999.99,
-      additional_check_props: "string",
-      cashier: "string",
-      additional_user_props: {
-        name: "string",
-        value: "string"
-      },
-      operating_check_props: {
-        name: "string",
-        value: "string",
-        timestamp: "string",
-      },
-      sectoral_check_props: [{
-        federal_id: "string",
-        date: "string",
-        number: "string",
-        value: "string",
-      }]
-    }
-  },
-  PaymentDetails: {
-    Value: "",
-  },
-  PaymentMethod: "CryptogramRSA",
-}
-
-
-const cryptogramValueData = {
-  TransactionDetails: {
-    Amount: "11.00",
-    Currency: "RUB",
-  },
-  PaymentDetails: {
-    CardholderName: "Card Holder",
-    CardNumber: "4111111111111111",
-    CVC: "603",
-    ExpMonth: "04",
-    ExpYear: "44"
-  },
-  PaymentMethod: "CryptogramRSA",
-  MessageExpiration: Date.now() + 86400000,
-}
-
 const requiredInputsFieldsList = [
   { label: "OrderId", value: MockCryptogramPaymentDataFFD1_05.OrderId, key: "OrderId" },
   { label: "Amount", value: MockCryptogramPaymentDataFFD1_05.Amount, key: "Amount" },
@@ -415,28 +125,41 @@ const extraDataInputsFieldsList = [
 ]
 
 export default function HomeScreen() {
-  const [publicPayResult, setPublicPayResult] = useState<PayResponse>({});
+  const [publicPayResult, setPublicPayResult] = useState<PayResponse>(MockPayResponse);
   const [transactionIdStatus, setTransactionIdStatus] =
       useState<MultiStateTransactionInfo
               | TransactionStateDeclined
               | TransactionStateWaitFor3ds
-              | TransactionStateRedirect>({});
+              | TransactionStateRedirect>(MockTransactionStatus);
   const [orderIdStatus, setOrderIdStatus] = useState<MultiStateTransactionInfo
       | TransactionStateDeclined
       | TransactionStateWaitFor3ds
-      | TransactionStateRedirect>({});
+      | TransactionStateRedirect>(MockTransactionStateWaitFor3ds);
   const [selectedButton, setSelectedButton] = useState<string>('1.05');
   
   let initialState = MockCryptogramPaymentDataFFD1_05;
   const handlePress = (value: string) => {
     setSelectedButton(value);
   };
-  const createPayment = async (values: CryptogramRSAPayment ) => {
+  const createPayment = async (values: CryptogramPayment ) => {
     
-    const publicKey="LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFxbk56eXlwR1R6cENZeDlzMnh6RQpaQ3B4eVkyL2YrbWljb0drWW1rV0M5Szl2SlIvcEtUL0VOUThCT1hIRkFBYW5Mb05PNzVLcmJUQ3Z5Y2pXbkJGCnhYQlJZY2NWeGsxaVB0c0VKbkNlcThmYXMwa2dYMFgzLzFnTFdvbkhheVdTUUl5emFTMXMrWUdsNEJyd2s5c08KTTQyZlk0dkM1WGptU1YxUDNlN2pvNUN1d2hxL2ljUkxZbTg1MXBYRTRiZ3FZYS96NEsrbXhUcWJvdC94b3lhTQpxSmlIOS9EUnQveTc0Z2t6Q0VIRThGQ0M4TkJlVXZUckRWbnlSQ0dtSlpVTDh0QnhPd1N3Ty94M1lzZi9CNU9vCkVjbllWdjVSQmF1MDl4VmFGTFN5QkZiRUsvZnRDUktFeUNQbnpYS2FnbTQ3T2dROEIvTkdhQ0cxRmdVOUhJb2gKd093TmsycWY1NTRPR21Oa0E3MnZCR1E0RTZ4TldUSnFJQWhOTUJQTjFMZGdRNXZTamszTUVJRHQ3Y3FEZzhFRwpCNU0vVS9VT2lVU2tXWmFtR3pXOVZFbkJhRFdWZFpxVVpTc0d0aCtJM093NGRPUUxiZG4rdzljYlpHLzR2VmwvCmFKdTdlQlZ2WVhEL0o0TnIzMk5RZ1o2YzlpMCtNU3RwWFUxMlJ4bzhJK1hCNVpZUTkzNE5iVXJoeDBuMlJhQk0KbGtlSTFtbE1ncWI3ME9BRk5zaDUyNUFIL3k5OVpJTzhsR0RqVEpSdDlKZzdGNVFmUEVWekRIbXdxdy9FaFFjQwpjVG5QaGRLOE53NDJ3QldIVDhXYXg4Y1NxYTdwRytTM2JOYkZvUVJlU1dvK2pzV0JNOU1NemJvckNqYWE1UzRNCnNCV0UyN2FRSElVMU5sTGNqK0laUldzQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=";
+    const publicRSAKey="LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFxbk56eXlwR1R6cENZeDlzMnh6RQpaQ3B4eVkyL2YrbWljb0drWW1rV0M5Szl2SlIvcEtUL0VOUThCT1hIRkFBYW5Mb05PNzVLcmJUQ3Z5Y2pXbkJGCnhYQlJZY2NWeGsxaVB0c0VKbkNlcThmYXMwa2dYMFgzLzFnTFdvbkhheVdTUUl5emFTMXMrWUdsNEJyd2s5c08KTTQyZlk0dkM1WGptU1YxUDNlN2pvNUN1d2hxL2ljUkxZbTg1MXBYRTRiZ3FZYS96NEsrbXhUcWJvdC94b3lhTQpxSmlIOS9EUnQveTc0Z2t6Q0VIRThGQ0M4TkJlVXZUckRWbnlSQ0dtSlpVTDh0QnhPd1N3Ty94M1lzZi9CNU9vCkVjbllWdjVSQmF1MDl4VmFGTFN5QkZiRUsvZnRDUktFeUNQbnpYS2FnbTQ3T2dROEIvTkdhQ0cxRmdVOUhJb2gKd093TmsycWY1NTRPR21Oa0E3MnZCR1E0RTZ4TldUSnFJQWhOTUJQTjFMZGdRNXZTamszTUVJRHQ3Y3FEZzhFRwpCNU0vVS9VT2lVU2tXWmFtR3pXOVZFbkJhRFdWZFpxVVpTc0d0aCtJM093NGRPUUxiZG4rdzljYlpHLzR2VmwvCmFKdTdlQlZ2WVhEL0o0TnIzMk5RZ1o2YzlpMCtNU3RwWFUxMlJ4bzhJK1hCNVpZUTkzNE5iVXJoeDBuMlJhQk0KbGtlSTFtbE1ncWI3ME9BRk5zaDUyNUFIL3k5OVpJTzhsR0RqVEpSdDlKZzdGNVFmUEVWekRIbXdxdy9FaFFjQwpjVG5QaGRLOE53NDJ3QldIVDhXYXg4Y1NxYTdwRytTM2JOYkZvUVJlU1dvK2pzV0JNOU1NemJvckNqYWE1UzRNCnNCV0UyN2FRSElVMU5sTGNqK0laUldzQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=";
+    const publicKey="04bd07d3547bd1f90ddbd985feaaec59420cabd082ff5215f34fd1c89c5d8562e8f5e97a5df87d7c99bc6f16a946319f61f9eb3ef7cf355d62469edb96c8bea09e\n";
     
-    const cryptogram = getCryptogramValue(cryptogramValueData, publicKey);
-    values.PaymentDetails.Value = cryptogram;
+    //for CryptogramRSA
+    
+    //const cryptogram = getCryptogramRSAValue(cryptogramRSAValueData, publicRSAKey);
+    //values.PaymentDetails.Value = cryptogram;*/
+    
+    
+    //for Cryptogram
+    try {
+      const cryptogram = getCryptogramECDHValue(cryptogramECDHValueData, publicKey);
+      values.PaymentDetails.Value = await cryptogram;
+    } catch (error) {
+      console.error('Encryption failed:', error);
+    }
+    
     
     const result = await paymentApi.publicPay(values, payHeader);
     setPublicPayResult(result);
---
+++ constants/mockData.ts
@@ -0,0 +1,347 @@
+import {
+    AgentInfoType,
+    PaymentMethodType, PaymentObjectFFD1_05, PaymentObjectFFD1_2, PaymentsType,
+    TaxSystem, VatType,
+} from "payselection-pay-app-sdk-reactnative/src/types/commonTypes";
+import { CryptogramPayment } from "payselection-pay-app-sdk-reactnative/src/types/payment/paymentPayload";
+import { PayResponse } from "payselection-pay-app-sdk-reactnative/src/types/payment/paymentResponse";
+import {
+    TransactionStateDeclined,
+    TransactionStateWaitFor3ds,
+} from "payselection-pay-app-sdk-reactnative/src/types/status/statusResponse";
+
+export const MockCryptogramPaymentDataFFD1_05: CryptogramPayment = {
+    OrderId: "SAM_SDK_3",
+    Amount: "11.00",
+    Currency: "RUB",
+    Description: "test payment",
+    RebillFlag: false,
+    CustomerInfo: {
+        Email: "user@example.com",
+        Phone: "+19991231212",
+        Language: "en",
+        Address: "string",
+        Town: "string",
+        ZIP: "1234567",
+        Country: "USA",
+        IP: "10.0.2.56",
+        UserId: "string"
+    },
+    ExtraData: {
+        ReturnUrl: "https://api.payselection.com/",
+        WebhookUrl: "https://webhook.site/94a06b69",
+        ScreenHeight: "768",
+        ScreenWidth: "1024",
+        ChallengeWindowSize: "5",
+        TimeZoneOffset: "-180",
+        ColorDepth: "8",
+        Region: "ru",
+        UserAgent: "Mozilla/5.0+(Macintosh;+Intel+Mac+OS+X+10_15_5)+AppleWebKit/527.36+(KHTML,+Gecko)+Chrome83.0.4103.116+Safari/537.36",
+        acceptHeader: "text/html",
+        JavaEnabled: true,
+        javaScriptEnabled: true
+    },
+    ReceiptData: {
+        timestamp: "string",
+        external_id: "string",
+        receipt: {
+            client: {
+                name: "string",
+                inn: "string",
+                email: "string",
+                phone: "string"
+            },
+            company: {
+                email: "string",
+                sno: TaxSystem.osn,
+                inn: "string",
+                payment_address: "string",
+            },
+            agent_info: {
+                type: AgentInfoType.bank_paying_agent,
+                paying_agent: {
+                    operation: "string",
+                    phones: ["+375298763261"],
+                },
+                receive_payments_operator: {
+                    phones: ["+375298763262"]
+                },
+                money_transfer_operator: {
+                    phones: ["+375298763263"],
+                    name: "string",
+                    address: "string",
+                    inn: "string"
+                }
+            },
+            supplier_info: {
+                phones: ["375298763264"]
+            },
+            items: [
+                {
+                    name: "string",
+                    price: 42949673,
+                    quantity: 99999.999,
+                    sum: 42949672.95,
+                    measurement_unit: "string",
+                    payment_method: PaymentMethodType.full_payment,
+                    payment_object: PaymentObjectFFD1_05.commodity,
+                    nomenclature_code: "string",
+                    vat: {
+                        type: VatType.vat10,
+                        sum: 99999999.99,
+                    },
+                    agent_info: {
+                        type: AgentInfoType.bank_paying_agent,
+                        paying_agent: {
+                            operation: "string",
+                            phones: ["+375441238751"]
+                        },
+                        receive_payments_operator: {
+                            phones: ["+375441238752"]
+                        },
+                        money_transfer_operator: {
+                            phones: ["+375441238734"],
+                            name: "string",
+                            address: "string",
+                            inn: "string",
+                        },
+                    },
+                    supplier_info: {
+                        phones: ["+375441238731"],
+                        name: "string",
+                        inn: "string",
+                    },
+                    user_data: "string",
+                    excise: 0,
+                    country_code: "str",
+                    declaration_number: "string",
+                }
+            ],
+            payments: [{
+                type: PaymentsType.cash,
+                sum: 99999999.99,
+            }],
+            vats: [{
+                type: VatType.vat0,
+                sum: 99999999.99,
+            }],
+            total: 99999999.99,
+            additional_check_props: "string",
+            cashier: "string",
+            additional_user_props: {
+                name: "string",
+                value: "string"
+            }
+        }
+    },
+    PaymentDetails: {
+        Value: "",
+    },
+    PaymentMethod: "Cryptogram",
+}
+
+export const MockCryptogramPaymentDataFFD1_2: CryptogramPayment = {
+    OrderId: "SAM_SDK_3",
+    Amount: "11.00",
+    Currency: "RUB",
+    Description: "test payment",
+    RebillFlag: false,
+    CustomerInfo: {
+        Email: "user@example.com",
+        Phone: "+19991231212",
+        Language: "en",
+        Address: "string",
+        Town: "string",
+        ZIP: "1234567",
+        Country: "USA",
+        IP: "10.0.2.56",
+        UserId: "string"
+    },
+    ExtraData: {
+        ReturnUrl: "https://api.payselection.com/",
+        WebhookUrl: "https://webhook.site/94a06b69",
+        ScreenHeight: "768",
+        ScreenWidth: "1024",
+        ChallengeWindowSize: "5",
+        TimeZoneOffset: "-180",
+        ColorDepth: "8",
+        Region: "ru",
+        UserAgent: "Mozilla/5.0+(Macintosh;+Intel+Mac+OS+X+10_15_5)+AppleWebKit/527.36+(KHTML,+Gecko)+Chrome83.0.4103.116+Safari/537.36",
+        acceptHeader: "text/html",
+        JavaEnabled: true,
+        javaScriptEnabled: true
+    },
+    ReceiptData: {
+        timestamp: "string",
+        external_id: "string",
+        receipt: {
+            client: {
+                name: "string",
+                inn: "string",
+                email: "string",
+                phone: "string"
+            },
+            company: {
+                email: "string",
+                sno: TaxSystem.osn,
+                inn: "string",
+                payment_address: "string",
+            },
+            items: [
+                {
+                    name: "string",
+                    price: 42949673,
+                    quantity: 99999.999,
+                    sum: 42949672.95,
+                    measure: 0,
+                    payment_method: PaymentMethodType.full_payment,
+                    payment_object: PaymentObjectFFD1_2.pawn,
+                    vat: {
+                        type: VatType.vat10,
+                        sum: 99999999.99,
+                    },
+                    agent_info: {
+                        type: AgentInfoType.bank_paying_agent,
+                        paying_agent: {
+                            operation: "string",
+                            phones: ["+375441238751"]
+                        },
+                        receive_payments_operator: {
+                            phones: ["+375441238752"]
+                        },
+                        money_transfer_operator: {
+                            phones: ["+375441238734"],
+                            name: "string",
+                            address: "string",
+                            inn: "string",
+                        },
+                    },
+                    supplier_info: {
+                        phones: ["+375441238731"],
+                        name: "string",
+                        inn: "string",
+                    },
+                    user_data: "string",
+                    excise: 0,
+                    country_code: "str",
+                    declaration_number: "string",
+                    mark_quantity: {
+                        numerator: 2,
+                        denominator: 2
+                    },
+                    mark_processing_mode: "string",
+                    sectoral_item_props: [{
+                        federal_id: "string",
+                        date: "string",
+                        number: "string",
+                        value: "string"
+                    }],
+                    mark_code: {
+                        unknown: "string",
+                        ean: "string",
+                        ean13: "string",
+                        itf14: "string",
+                        gs10: "string",
+                        gs1m: "string",
+                        short: "string",
+                        fur: "string",
+                        egais20: "string",
+                        egais30: "string",
+                    }
+                }
+            ],
+            payments: [{
+                type: PaymentsType.cash,
+                sum: 99999999.99,
+            }],
+            vats: [{
+                type: VatType.vat0,
+                sum: 99999999.99,
+            }],
+            total: 99999999.99,
+            additional_check_props: "string",
+            cashier: "string",
+            additional_user_props: {
+                name: "string",
+                value: "string"
+            },
+            operating_check_props: {
+                name: "string",
+                value: "string",
+                timestamp: "string",
+            },
+            sectoral_check_props: [{
+                federal_id: "string",
+                date: "string",
+                number: "string",
+                value: "string",
+            }]
+        }
+    },
+    PaymentDetails: {
+        Value: "",
+    },
+    PaymentMethod: "Cryptogram",
+}
+
+export const MockTransactionStateWaitFor3ds: TransactionStateWaitFor3ds = {
+    TransactionState: "",
+    TransactionId: "",
+    OrderId: "",
+    StateDetails: {
+        AcsUrl: "",
+        PaReq: "",
+        MD: "",
+    }
+}
+
+export const MockPayResponse: PayResponse = {
+    TransactionId: "",
+    OrderId: "",
+    Amount: 0,
+    Currency: "",
+    RedirectUrl: "",
+    TransactionSecretKey: "",
+}
+
+export const MockTransactionStatus: TransactionStateDeclined = {
+    TransactionState: "",
+    TransactionId: "",
+    OrderId: "",
+    StateDetails: {
+        Code: "",
+        Description: "",
+    }
+}
+
+const cryptogramRSAValueData = {
+    TransactionDetails: {
+        Amount: "11.00",
+        Currency: "RUB",
+    },
+    PaymentDetails: {
+        CardholderName: "Card Holder",
+        CardNumber: "4111111111111111",
+        CVC: "603",
+        ExpMonth: "04",
+        ExpYear: "44"
+    },
+    PaymentMethod: "CryptogramRSA",
+    MessageExpiration: Date.now() + 86400000,
+}
+
+export const cryptogramECDHValueData = {
+    TransactionDetails: {
+        Amount: "11.00",
+        Currency: "RUB",
+    },
+    PaymentDetails: {
+        CardholderName: "Card Holder",
+        CardNumber: "4111111111111111",
+        CVC: "603",
+        ExpMonth: "04",
+        ExpYear: "44"
+    },
+    PaymentMethod: "Cryptogram",
+    MessageExpiration: Date.now() + 86400000,
+}
